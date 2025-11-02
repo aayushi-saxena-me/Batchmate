47ea01720c2f21be440cc467e8937cff0b9360a2
aayushi-saxena-me <aayushi.saxena.me@gmail.com>
2025-11-02 08:20:41 -0500

    Remove all PORT variable references - use fixed port 8000

--- start.sh
+++ start.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 set -e
 
-# Use PORT environment variable if set, otherwise default to 8000
-PORT=${PORT:-8000}
-
-# Run migrations (optional - can be done manually)
-# python manage.py migrate --noinput || true
-
-# Start gunicorn
+# Start gunicorn on port 8000
 exec gunicorn inventory_management.wsgi:application \
     --bind "0.0.0.0:8000" \
     --workers 2 \
